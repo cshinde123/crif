221c58923a4931f746b0a46877330e3faa743b7f
Shinde Chetan <in207cheshi@ext.crifnet.com>
2019-01-30 15:38:17 +0530

    Test Push

--- crif-library/src/main/java/com/crif/android/crif_library/CRIFData.java
+++ crif-library/src/main/java/com/crif/android/crif_library/CRIFData.java
@@ -32,7 +32,7 @@ public class CRIFData {
 
     public static DownloadService localService;
     public static boolean isBound = false;
-    public static String HOME_URL = "http://18.191.145.152/api/";
+    public static String HOME_URL = "http://192.168.43.147/api/";
     public static String EMAIL_URL = "Emailtbs/PostEmailtb";
     public static Context contextService;
     public static String mobileNo, time;
